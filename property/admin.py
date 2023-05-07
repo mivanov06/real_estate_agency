efe8a674fc10eae2f48b375f05c0bf344fd90610
mivanov06 <mivanov06@yandex.ru>
2023-05-08 00:18:30 +1000

    added inline for AdminFlat

--- property/admin.py
+++ property/admin.py
@@ -3,13 +3,29 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class ModelInInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner', 'flat')
+
+
 class AdminFlat(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building']
     raw_id_fields = ['liked_by']
+    inlines = [
+        ModelInInline,
+    ]
+    fieldsets = (
+        ('Адрес', {
+            'fields': ('town', 'town_district', 'address', 'floor')
+        }),
+        ('Информация о квартире', {
+            'fields': ('rooms_number', 'living_area', 'has_balcony', 'construction_year', 'new_building')
+        })
+    )
 
 
 class AdminComplaint(admin.ModelAdmin):
